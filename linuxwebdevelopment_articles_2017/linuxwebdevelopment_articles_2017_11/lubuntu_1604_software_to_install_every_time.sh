c37b3d84e09bb31e8cea6cb06808499498168cef
linuxwebdevelopment <33786288+linuxwebdevelopment@users.noreply.github.com>
2017-11-19 01:18:03 +0800

    Create lubuntu_1604_software_to_install_every_time.sh

---
+++ linuxwebdevelopment_articles_2017/linuxwebdevelopment_articles_2017_11/lubuntu_1604_software_to_install_every_time.sh
@@ -0,0 +1,59 @@
+#!/bin/bash/
+#This file was created by linuxwebdevelopment.com
+#It is released under the GPLv3 license
+#You are free to copy it, modify it, and/or share it
+#Just please give credit to linuxwebdevelopment.com
+#this file is from the article called ... located at ....
+
+sudo apt-get install alarm-clock-applet \
+apache2 \
+arandr \
+bum \
+chromium-browser \
+clamav \
+curl \
+deluge \
+dillo \
+elinks \
+espeak \
+fbreader \
+fdupes \
+filezilla \
+freeplane \
+gimp \
+git \
+gnome-screenshot \
+gparted \
+hunspell hunspell-en-us myspell-en-gb \
+ipython3 \
+keepassx \
+libreoffice \
+lynx \
+mariadb-server \
+mnemosyne \4_software_to_install_every_time.sh
+nodejs \
+npm \
+openvpn \
+pavucontrol \
+phatch \
+php7.0 \
+pinta \
+pluma \
+pulseaudio \
+pwgen \
+qemu \
+qemu-kvm \
+shotwell \
+simple-image-reducer \
+soundconverter \
+spyder3 \
+sshfs \
+stopwatch \
+sysinfo \
+thunderbird \
+vim \
+vinagre \
+vlc \
+whois \
+wine \
+youtube-dl
