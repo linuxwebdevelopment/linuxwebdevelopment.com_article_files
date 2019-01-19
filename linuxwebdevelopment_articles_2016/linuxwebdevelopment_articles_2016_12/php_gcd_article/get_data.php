4f31f473f46dcaa6fa17a0949579d1f6305be738
linuxwebdevelopment <33786288+linuxwebdevelopment@users.noreply.github.com>
2019-01-19 22:25:16 +0800

    Create get_data.php

---
+++ linuxwebdevelopment_articles_2016/linuxwebdevelopment_articles_2016_12/php_gcd_article/get_data.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <p>This program will calculate the gcd of your given 2 integers.</p>
+    <form action="calculate_gcd.php" method="post">
+      Enter your first integer  <input type="text" name="integer1" /><br />
+      Enter your second integer <input type="text" name="integer2" /><br />
+      <input type="submit" name="submit" value="Calculate!" />
+    </form>
+  </body>
+</html>
