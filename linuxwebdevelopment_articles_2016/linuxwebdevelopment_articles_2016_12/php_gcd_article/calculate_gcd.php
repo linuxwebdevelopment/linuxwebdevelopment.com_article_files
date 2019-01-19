45d58836bffb21333fcdaca5177c72f96684ba60
linuxwebdevelopment <33786288+linuxwebdevelopment@users.noreply.github.com>
2019-01-19 22:43:26 +0800

    Create calculate_gcd.php

---
+++ linuxwebdevelopment_articles_2016/linuxwebdevelopment_articles_2016_12/php_gcd_article/calculate_gcd.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>Get GCD of two integers PHP</title>
+  </head>
+  <body>
+  <?php include 'gcd.php';
+  $first_integer = $_POST['integer1'];
+  $second_integer= $_POST['integer2'];
+  $calculated_gcd = gcd($first_integer, $second_integer);
+
+  echo "the gcd of " . $first_integer . " and " . $second_integer . " is " . $calculated_gcd;
+  echo "<br>Calculate again? <a href='get_data.php'>go back </a>";
+  ?>
+  </body>
+</html>
